c833243481846eeae5b25fea9d0cd5470a7f353c
omricarmi <omricarmi@gmail.com>
2017-06-12 17:00:24 +0300

    Clion init

---
+++ cmake-build-debug/CMakeFiles/HM_16_14.dir/DependInfo.cmake
@@ -0,0 +1,99 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/libmd5/libmd5.c" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/libmd5/libmd5.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/App/TAppDecoder/TAppDecCfg.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/App/TAppDecoder/TAppDecCfg.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/App/TAppDecoder/TAppDecTop.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/App/TAppDecoder/TAppDecTop.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/App/TAppDecoder/decmain.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/App/TAppDecoder/decmain.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/App/TAppEncoder/TAppEncCfg.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/App/TAppEncoder/TAppEncCfg.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/App/TAppEncoder/TAppEncTop.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/App/TAppEncoder/TAppEncTop.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/App/TAppEncoder/encmain.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/App/TAppEncoder/encmain.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/App/utils/BitrateTargeting/ExtractBitrates.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/App/utils/BitrateTargeting/ExtractBitrates.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/App/utils/BitrateTargeting/ExtractBitratesMain.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/App/utils/BitrateTargeting/ExtractBitratesMain.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/App/utils/BitrateTargeting/GuessLambdaModifiers.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/App/utils/BitrateTargeting/GuessLambdaModifiers.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/App/utils/BitrateTargeting/GuessLambdaModifiersMain.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/App/utils/BitrateTargeting/GuessLambdaModifiersMain.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/App/utils/annexBbytecount.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/App/utils/annexBbytecount.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/App/utils/convert_NtoMbit_YCbCr.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/App/utils/convert_NtoMbit_YCbCr.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TAppCommon/program_options_lite.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TAppCommon/program_options_lite.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/ContextModel.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/ContextModel.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/ContextModel3DBuffer.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/ContextModel3DBuffer.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/Debug.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/Debug.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/SEI.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/SEI.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComBitStream.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComBitStream.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComCABACTables.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComCABACTables.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComChromaFormat.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComChromaFormat.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComDataCU.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComDataCU.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComInterpolationFilter.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComInterpolationFilter.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComLoopFilter.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComLoopFilter.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComMotionInfo.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComMotionInfo.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComPattern.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComPattern.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComPic.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComPic.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComPicSym.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComPicSym.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComPicYuv.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComPicYuv.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComPicYuvMD5.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComPicYuvMD5.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComPrediction.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComPrediction.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComRdCost.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComRdCost.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComRdCostWeightPrediction.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComRdCostWeightPrediction.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComRom.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComRom.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComSampleAdaptiveOffset.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComSampleAdaptiveOffset.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComSlice.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComSlice.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComTU.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComTU.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComTrQuant.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComTrQuant.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComWeightPrediction.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComWeightPrediction.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibCommon/TComYuv.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibCommon/TComYuv.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibDecoder/AnnexBread.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibDecoder/AnnexBread.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibDecoder/NALread.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibDecoder/NALread.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibDecoder/SEIread.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibDecoder/SEIread.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibDecoder/SyntaxElementParser.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibDecoder/SyntaxElementParser.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibDecoder/TDecBinCoderCABAC.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibDecoder/TDecBinCoderCABAC.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibDecoder/TDecCAVLC.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibDecoder/TDecCAVLC.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibDecoder/TDecCu.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibDecoder/TDecCu.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibDecoder/TDecEntropy.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibDecoder/TDecEntropy.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibDecoder/TDecGop.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibDecoder/TDecGop.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibDecoder/TDecSbac.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibDecoder/TDecSbac.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibDecoder/TDecSlice.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibDecoder/TDecSlice.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibDecoder/TDecTop.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibDecoder/TDecTop.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/NALwrite.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/NALwrite.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/SEIEncoder.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/SEIEncoder.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/SEIwrite.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/SEIwrite.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/SyntaxElementWriter.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/SyntaxElementWriter.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/TEncBinCoderCABAC.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/TEncBinCoderCABAC.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/TEncBinCoderCABACCounter.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/TEncBinCoderCABACCounter.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/TEncCavlc.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/TEncCavlc.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/TEncCu.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/TEncCu.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/TEncEntropy.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/TEncEntropy.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/TEncGOP.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/TEncGOP.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/TEncPic.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/TEncPic.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/TEncPreanalyzer.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/TEncPreanalyzer.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/TEncRateCtrl.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/TEncRateCtrl.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/TEncSampleAdaptiveOffset.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/TEncSampleAdaptiveOffset.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/TEncSbac.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/TEncSbac.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/TEncSearch.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/TEncSearch.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/TEncSlice.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/TEncSlice.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/TEncTop.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/TEncTop.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibEncoder/WeightPredAnalysis.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibEncoder/WeightPredAnalysis.cpp.o"
+  "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/source/Lib/TLibVideoIO/TVideoIOYuv.cpp" "/Users/omricarmi/Technion/Project A/T-REC-H/Software/HM-16.14/cmake-build-debug/CMakeFiles/HM_16_14.dir/source/Lib/TLibVideoIO/TVideoIOYuv.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/HM_16_14.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HM_16_14.
+# This may be replaced when dependencies are built.
